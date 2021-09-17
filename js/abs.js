81391379c342bbe933d7425ad15185c61d8f42b6
Steven Madi <Steven.madi@edu.devinci.fr>
2021-09-17 12:18:28 +0200

    Modify ABS

---
+++ js/abs.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('.btn-warning')[0].classList.add('.btn-warningnew')
+
+document.getElementsByTagName('.label-warning')[0].classList.add('.label-warningnew')
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 document.getElementsByTagName('body')[0].classList.add('override_body')
+
+document.getElementsByTagName('avatar')[0].classList.add('newavatar')
+
+
+
+
+
+
