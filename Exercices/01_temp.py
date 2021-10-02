030b534211c1352a7b0011f15b9f7faaad6161ad
Guillaume Fine <guillaume.fine@edu.devinci.fr>
2021-10-02 15:28:07 +0200

    Delete Exercices directory

--- Exercices/01_temp.py
+++
@@ -1,8 +0,0 @@
-temp = float(input("What's your temp?"))
-
-if temp < 36:
-    print("Hypothermia")
-elif temp >= 36 and temp < 38:
-    print("Ok")
-else:
-    print("Fever")
--- Exercices/02_sum.py
+++
@@ -1,7 +0,0 @@
-def sum(n):
-    result = 0
-    for i in range(1, n+1):
-        result += i
-    return result
-
-print(sum(int(input("Un nombre entier:"))))
