0097ccc8d16a626cdabeebb3749e58b9878ccb6a
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-01 10:11:20 +0100

    :hammer: Update all page globally and style.scss

--- js/header.js
+++ js/header.js
@@ -1,7 +1,12 @@
-let header = document.getElementsByTagName('header')[0];
-const className = "toggle";
+const header = document.getElementsByTagName('header')[0]; // We pick up the header or other element we want to change on scroll
+const className = 'toggle'; // The name of the class we want to add
+const scrollTrigger = 500; // Moment that we want to add class 
 
-window.addEventListener('scroll', function() {
-    let windowposition = window.screenY > 0;
-    header.classList.toggle(className, windowposition);
-})
+document.addEventListener('scroll', function() {
+    const top = document.documentElement.scrollTop; // We check at how many pixels we are from the top 
+    if (top > scrollTrigger) { // We compare the value we actually have with the one we want 
+        header.classList.add(className); // a,d if it's taller than the one we want, we add the class to header
+    }else { // and if it's become smaller we removed the class.
+        header.classList.remove(className);
+    }
+});
