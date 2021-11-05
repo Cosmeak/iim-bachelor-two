d3bdd3d1cdb69c151cf13ce9245dfab35a20a2ac
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-05 10:41:27 +0100

    :hammer: Update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,17 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        header: '67px',
+        headerTop: '96px',
+        headerBottom: '67px',
       },
       colors: {
         'blue-kleber': 'rgba(15, 49, 175, 1)',
         'darkblue-kleber': '#0C288D',
         'yellow-kleber': 'rgba(255, 214, 0, 1)',
         'darkgray-kleber': '#383C43',
+      },
+      zIndex: {
+        'neg': -1,
       },
     },
   },
