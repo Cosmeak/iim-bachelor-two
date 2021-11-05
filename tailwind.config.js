431bb20c3d3347660bf88c20a7111715f4805364
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-05 10:07:02 +0100

    :hammer: Update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,15 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    height: {
-      header: '67px',
-    },
     extend: {
-      colors: {
-        'blue-kleber': 'rgba(15, 49, 175, 0.9)',
-        'darkblue-kleber': '#0C288D',
-        'yellow-kleber': 'rgba(255, 214, 0, 0.9)',
-        'darkgray-kleber': '#383C43',
+      height: {
+        header: '67px',
       },
-      backgroundImage: {
-        'texture': "url('/img/texture.jfif')",
-        'kleber-team': "url('/img/kine-kleber-team.png')",
+      colors: {
+        'blue-kleber': 'rgba(15, 49, 175, 1)',
+        'darkblue-kleber': '#0C288D',
+        'yellow-kleber': 'rgba(255, 214, 0, 1)',
+        'darkgray-kleber': '#383C43',
       },
     },
   },
