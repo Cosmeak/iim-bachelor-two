5088a0a0a8f2e6291716354005bf459388e1f3dd
Guillaume <guillaume.fine@edu.devinci.fr>
2022-01-02 00:50:58 +0100

    :hammer: add update function for player

--- Routes/apiRouter.js
+++ Routes/apiRouter.js
@@ -1,22 +1,25 @@
 // Imports
 const express = require('express');
-const gameObjectController = require('./gameObjectController');
 const router = express.Router();
 const userController = require('./userController');
+const gameObjectController = require('./gameObjectController');
 
 // Time logger
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog (request, response, next) {
   console.log('Request at: ', Date.now())
   next()
-})
+});
 
-// Players Routes
+// User Routes
 router.route('/user/').get(userController.getAllUser);
 router.route('/user/:idOrUsername').get(userController.getUserByIdOrName);
+router.route('/user/').post(userController.createUser);
+router.route('/user/:idOrUsername/').put(userController.updateUser);
+router.route('/user/:idOrUsername/').delete(userController.deleteUser);
 
 // Game Object Routes
 router.route('/game-object/').get(gameObjectController.getAllGameObject);
-router.route('/game-object/:idOrName').get(gameObjectController.getGameObjectByIdOrName);
+router.route('/game-object/:idOrName/').get(gameObjectController.getGameObjectByIdOrName);
 
 // Exports
 module.exports = router;
--- Routes/userController.js
+++ Routes/userController.js
@@ -50,5 +50,101 @@ module.exports = {
       response.status(500).json({ status: 'Failure', reason: error });
     });
   },
+
+  createUser: (request, response) => {
+    // Params 
+    const username = request.body.username;
+    const money = 0;
+    const resources = 0;
+    const workforces = 0;
+    const score = 0;
+    const createdAt = Date.now();
+    const updatedAt = Date.now();
+    
+    if(username == null) {
+      response.status(400).json({ status: 'Failure', reason: 'Missing username!'})
+      console.log(response);
+    };
+
+    // Query check if the user already exist
+    models.User.findOne({
+      attributes: ['username'],
+      where: { username: username}
+    })
+    .then (userFound => {
+      if(!userFound) {
+        // Query INSERT 
+        let newUser = models.User.create({
+          username: username,
+          money: money,
+          resources: resources,
+          workforces: workforces,
+          score: score,
+          createdAt: createdAt,
+          updatedAt: updatedAt
+        })
+        .then( newUser => {
+          response.status(201).json({ status: 'Success', data: newUser});
+        })
+        .catch( error => {
+          console.log(error);
+          response.status(500).json({ status: 'Failure', reason: error });
+        })
+          
+      } else {
+        response.status(409).json({ status: 'Failure', reason: 'Username already exist!'})
+      }
+    })
+    .catch( error => {
+      console.log(error);
+      response.status(500).json({ status: 'Failure', reason: error });
+    })
+  },
+
+  updateUser: (request, response) => {
+    // Params
+    const fields = request.query.fields;
+    const userIdOrName = request.params.idOrUsername;
+    const money = request.body.money;
+    const resources = request.body.resources;
+    const workforces = request.body.workforces;
+    const score = request.body.score;
+    const updatedAt = Date.now();
+
+    models.User.findOne({
+      attributes: (fields !== '*' && fields != null) ? fields.split(',') : null,
+      where: { [Op.or]: [ { id: userIdOrName }, { username: userIdOrName } ] }
+    })
+    .then( userFound => {
+      if(userFound){
+        userFound.update({
+          money: (money ? money : userFound.money),
+          resources: (resources ? resources : userFound.resources),
+          workforces: (workforces ? workforces : userFound.workforces),
+          score: (score ? score : userFound.score),
+          updatedAt: updatedAt
+        })
+        .then( userUpdate => {
+          response.status(200).json({ status: 'Success', User: userUpdate});
+        })
+        .catch( error => {
+          console.log(error);
+          response.status(500).json({ status: 'Failure', reason: 'Cannot update this user!' });
+        })
+
+      } else {
+        response.status(404).json({ status: 'Failure', reason: 'User not found!' });
+      };
+    })
+    .catch( error => {
+      console.log(error);
+      response.status(500).json({ status: 'Failure', reason: 'Unable to verify user!' });
+    })
+  },
+
+  deleteUser: (request, response) => {
+    //Params
+    
+  }
   
 };
