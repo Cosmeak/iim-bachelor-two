61e704264ad1b57db27f38fa5656b36933ed967b
Cosmeak <guillaume.fine@edu.devinci.fr>
2022-02-11 10:59:05 +0100

    :hammer: Update lp

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/28c63ecd3f.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+  <title>Les formations Coding & Digital Innovation</title>
+</head>
+<body>
+  <div class="deco-top">
+    <div class='dotted-circle'>
+      <div class='circle'>
+        <img src='img/Logo_IIM_fd_blanc.png'>
+      </div>
+    </div>
+    <h1>Découvrir les formations Coding & Digital Innovation </h1>
+  </div>
+
+
+  <section class="first">
+    <div class='part1'>
+      <div class='learn'>
+          <h2>Apprenez à faire vos propres sites Web et les mettres en lignes.</h2>
+          <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <div class='button'>
+          <a class='button_orange' href ="#form">S'inscrire</a>
+          <a class='button_white' href ="#formations">Voir nos formations</a>
+        </div>
+      </div>
+      <div class='test'> 
+        <div class='circle1'>
+          <i class="fab fa-css3-alt relative testing"></i>
+          <i class="fab fa-js relative "></i>
+          <i class="fab fa-php relative "></i>
+          <div class='circle2'>
+            <div class='circle3'>
+              <div class='circle4'><p>💻</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="second" id="formations">
+    
+  </section>
+
+  <section class="third" id="form">
+    <h2>S'inscrire à nos formations</h2>
+    <form action="" method="POST">
+        <input type="text" name="firstname" placeholder="Votre prénom.." required>
+        <input type="text" name="lastname" placeholder="Votre nom de famille..." required>
+        <input type="email" name="email" placeholder="Votre email..." required>
+        <select name="formation" required>
+          <option value="">Selectionner une formation</option>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
+  </section>
+
+  <footer>
+        <div class="footer_symbole">
+            <article class="footer_symbole_article">
+                <img src="footer_one.png" alt="img">
+                <h3></h3>
+                <p class="footer_symbole_texte"></p>
+            </article>
+            <article class="footer_symbole_article">
+                <img src="footer_two.png" alt="img_classement">
+                <h3>Numéro 1 des école française du digital</h3>
+                <p class="footer_symbole_texte">Les % de nos élèves trouvent un travail apres
+                    leur diplome avec un salaire moyen de k.</p>
+            </article>
+            <article class="footer_symbole_article">
+                <img src="footer_three.png" alt="img_mastère">
+                <h3>2 Mastères reconnu par l’état</h3>
+                <p class="footer_symbole_texte">Nos élèves auront le choix entre
+                    Ingénierie web et mobile
+                    et un double diplôme
+                    Management de la transformation digitale
+                </p>
+            </article>
+        </div>
+        <div class="footer_mentions">
+            <div class="footer_mentions_title">
+                <h2>Afficher les mentions légales</h2>
+            </div>
+            <div class="footer_mentions_hr">
+                <div class="foooter_hr"></div>
+            </div>
+            <div class="footer_mentions_text">
+                <p>Lorem ipsum dolor sit amet. Et voluptas quidem et dignissimos tempore ea voluptatem aliquam sit quia tempora eum galisum explicabo. Qui harum voluptates est harum deleniti est consequatur accusantium sed suscipit similique. Eum error minus aut ducimus dolorem ea provident commodi rem blanditiis quis et officiis explicabo sit necessitatibus voluptate eum enim tenetur.  Et nobis explicabo in autem inventore 33 consequatur explicabo sit veniam numquam et Quis ducimus.</p>
+                <p>Non galisum neque vel minima omnis et quis quos aut suscipit omnis eum magnam illum. Hic amet enim est obcaecati omnis sed magni excepturi qui ullam omnis et exercitationem deleniti qui sunt amet.
+                    Vel consequuntur atque aut quasi rerum est consequatur aperiam eum voluptatem ipsum! Ut delectus possimus in doloribus voluptatem in enim facilis.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
