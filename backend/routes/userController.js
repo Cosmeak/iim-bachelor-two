122c6a07934eecba4b8bcfc36eeb95024af4a653
Cosmeak <guillaumefine09@gmail.com>
2022-02-21 14:45:31 +0100

    :heavy_minus_sign: Update dependencies

--- backend/routes/userController.js
+++ backend/routes/userController.js
@@ -1,10 +1,8 @@
 // Imports
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
 // Functions
-
 /**
 * Display a listing of the resource.
 *
