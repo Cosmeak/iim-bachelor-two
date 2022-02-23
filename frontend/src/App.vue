8fa3712041fe002e0afd5a5c9f5d847300b00906
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-02-23 09:42:22 +0100

    :tada: Add register

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <register />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from "./components/register";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Register
   }
 }
 </script>
