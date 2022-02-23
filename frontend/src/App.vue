7e56a35af3a0ec733d149581be6a7227e5809032
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-02-23 10:53:49 +0100

    :tada: Add register and connection components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,11 @@
 <template class="m-0 p-0">
-  <register />
 </template>
 
 <script>
-import Register from "./components/register";
 
 export default {
   name: 'App',
   components: {
-    Register
   }
 }
 </script>
--- frontend/src/components/connection.vue
+++ frontend/src/components/connection.vue
@@ -5,8 +5,7 @@
   <div class="flex justify-center mt-64 w-screen">
     <form>
       <div class="flex flex-col justify-center">
-        <input class="p-4 w-96 border-slate-700 rounded-t-xl border-2" name="username" v-model="username" placeholder="username">
-        <input class="p-4 w-96 border-slate-700 border-2" name="email" v-model="email" placeholder="email" type="email">
+        <input class="p-4 w-96 border-slate-700 border-2 rounded-t-xl" name="email" v-model="email" placeholder="email" type="email">
         <input class="p-4 w-96 border-slate-700 border-2 rounded-b-xl" name="password" v-model="password" placeholder="password">
       </div>
       <div class="mt-8 h-12 rounded-xl bg-gray-700 flex justify-center text-white">
@@ -21,7 +20,6 @@ export default {
   name: "register",
   data() {
     return {
-      username: "",
       email: "",
       password: "",
     };
