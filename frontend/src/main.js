312ae9ea562431364cc31082f316afbba1551aab
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-02-27 21:38:19 +0100

    :hammer: Update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
-createApp(App).use(router).mount('#app')
+import axios from "axios";
+import VueAxios from 'vue-axios'
+
+
+createApp(App).use(router, axios, VueAxios).mount('#app')
--- frontend/src/views/connection.vue
+++ frontend/src/views/connection.vue
@@ -1,47 +1,45 @@
 <template>
   <div class="bg-gray-700 text-white items-center text-3xl font-bold h-20 flex justify-center">
-    <h1>Créer un compte</h1>
+    <h1>Connexion à un compte</h1>
   </div>
   <div class="flex justify-center mt-64 w-screen">
-    <form>
+    <form @submit="validate">
       <div class="flex flex-col justify-center">
         <input class="p-4 w-96 border-slate-700 border-2 rounded-t-xl" v-model="email" placeholder="email" />
         <input class="p-4 w-96 border-slate-700 border-2 rounded-b-xl" v-model="password" placeholder="password" />
       </div>
       <div class="mt-8 h-12 rounded-xl bg-gray-700 flex justify-center text-white">
-        <buttonconnection @buttonClick="validate()" />
+        <!--<buttonconnection @buttonClick="validate()" />-->
+        <input
+            type="submit"
+            value="Submit"
+        >
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import Buttonconnection from "@/components/buttonconnection";
 
 export default {
   name: "register",
-  components: { Buttonconnection },
+  components: {  },
   data: () => ({
     email : [],
     password: []
   }),
   methods: {
-    validate: function () {
-      console.log("test");
-      this.axios
-          .get("localhost:3000/api/user")
-          .then((response) => {
-            console.log(response)
-            if (response.data.user) {
-              console.log(response.data.user);
-              console.log("biteee")
-            } else {
-              console.log("Not in this fucking fuck database aka db in french base de donnee")
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+    validate: function (e) {
+      console.log("ok")
+      e.preventDefault()
+      const axios = require('axios')
+
+
+      /*axios({ method: 'POST', url: url, data: body, headers: headers })
+          .then(response => console.log(response))*/
+      axios.post("http://localhost:3000/api/user")
+      .then(u => console.log(u))
+      .catch(err => console.log(err))
     }
   }
 };
