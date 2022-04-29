11400fbcb63fc388014327482f8d6cd22a728923
Oxyzal <steven.madi@edu.devinci.fr>
2022-04-29 11:48:58 +0200

    :hammer: waiting room add

--- screens/WaitingRoom.js
+++ screens/WaitingRoom.js
@@ -1,22 +1,64 @@
-import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+
+import React from 'react';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 
 class WaitingRoom extends React.Component{
-    render(){
-      return (
-        <View style={styles.container}>
-        <ImageBackground source={require('../assets/bg_cb.png') } resizeMode="cover" style={styles.image}>
-          <View style={style.player}/>
-        </ImageBackground>
-      </View> 
-      )
-    }
+  render(){
+  const [items, setItems] = React.useState([
+    { name: 'TURQUOISE', code: '#1abc9c' },
+    { name: 'EMERALD', code: '#2ecc71' },
+    { name: 'PETER RIVER', code: '#3498db' },
+    { name: 'AMETHYST', code: '#9b59b6' },
+    { name: 'WET ASPHALT', code: '#34495e' },
+    { name: 'GREEN SEA', code: '#16a085' },
+    { name: 'NEPHRITIS', code: '#27ae60' },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require('../assets/bg_cb.png') } resizeMode="cover" style={styles.image}>
+        <FlatGrid
+          itemDimension={7}
+          data={items}
+          style={styles.gridView}
+          // staticDimension={300}
+          // fixed
+          spacing={10}
+          renderItem={({ item }) => (
+            <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={styles.itemCode}>{item.code}</Text>
+            </View>
+        )}
+      />
+      </ImageBackground>
+    </View> 
+  );
+}
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
+  gridView: {
+    marginTop: 10,
+    flex: 1,
   },
-})
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
+  },
+});
 
 export default WaitingRoom;
