ce8a6ca3b6b5cd57b29631edaf625848a1155ea6
Oxyzal <steven.madi@edu.devinci.fr>
2022-04-29 12:22:53 +0200

    :bug; fixed

--- screens/WaitingRoom.js
+++ screens/WaitingRoom.js
@@ -1,10 +1,8 @@
-
 import React from 'react';
-import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground,Dimensions } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 
-class WaitingRoom extends React.Component{
-  render(){
+export default function WaitingRoom() {
   const [items, setItems] = React.useState([
     { name: 'TURQUOISE', code: '#1abc9c' },
     { name: 'EMERALD', code: '#2ecc71' },
@@ -12,42 +10,42 @@ class WaitingRoom extends React.Component{
     { name: 'AMETHYST', code: '#9b59b6' },
     { name: 'WET ASPHALT', code: '#34495e' },
     { name: 'GREEN SEA', code: '#16a085' },
-    { name: 'NEPHRITIS', code: '#27ae60' },
   ]);
 
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../assets/bg_cb.png') } resizeMode="cover" style={styles.image}>
-        <FlatGrid
-          itemDimension={7}
-          data={items}
-          style={styles.gridView}
-          // staticDimension={300}
-          // fixed
-          spacing={10}
-          renderItem={({ item }) => (
-            <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-              <Text style={styles.itemName}>{item.name}</Text>
-              <Text style={styles.itemCode}>{item.code}</Text>
-            </View>
-        )}
-      />
-      </ImageBackground>
-    </View> 
+    <ImageBackground source={require('../assets/game_bg.png') } resizeMode="cover" style={styles.image}>
+    <FlatGrid
+      itemDimension={100}
+      data={items}
+      style={styles.gridView}
+      // staticDimension={300}
+      // fixed
+      spacing={30}
+      renderItem={({ item }) => (
+        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text style={styles.itemCode}>{item.code}</Text>
+        </View>
+      )}
+    />
+    </ImageBackground>
   );
-}
 }
 
 const styles = StyleSheet.create({
+  image: {
+    height: Dimensions.get("window").height,
+},
   gridView: {
-    marginTop: 10,
+    marginTop: 170,
     flex: 1,
   },
   itemContainer: {
     justifyContent: 'flex-end',
     borderRadius: 5,
     padding: 10,
-    height: 150,
+    height: 164,
+    width: 164,
   },
   itemName: {
     fontSize: 16,
@@ -60,5 +58,3 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 });
-
-export default WaitingRoom;
