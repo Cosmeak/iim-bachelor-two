322c886722a2ccc9713273138fce6531e6704288
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2023-04-27 13:25:24 +0000

    feat: move files to a folder

---
+++ devlab/apps/backoffice/src/views/objects/Create.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'ObjectsIndex',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        create() {
+
+        }
+    }
+}
+</script>
---
+++ devlab/apps/mobile/screens/Admin.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { Dimensions, ImageBackground, StyleSheet, TextInput, View, Image } from "react-native";
+
+
+
+class Admin extends React.Component{
+  render(){
+    return (
+      <View style={styles.container}>
+      <ImageBackground source={require('../assets/bg_cb.png') } resizeMode="cover" style={styles.image}>
+        <View style={styles.rond}>
+          <Image
+            style={styles.logo}
+            source={require('../assets/logo_cb.png')}
+          />
+        </View>
+        <TextInput
+          style={[styles.inputt, margin.mt]}
+          placeholder="Email"
+          placeholderTextColor="#ffffff80"
+          underlineColorAndroid='transparent'
+        />
+        <TextInput
+          style={[styles.inputt, margin.mt5]}
+          placeholder="Mot de passe "
+          placeholderTextColor="#ffffff80"
+          underlineColorAndroid='transparent'
+        />
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </ImageBackground>
+    </View> 
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+},
+  image: {
+    height: Dimensions.get("window").height,
+},
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+},
+  rond: {
+    padding : 0,
+    borderRadius: 1000,
+    backgroundColor: "#000000c0",
+    overflow: 'hidden',
+    alignSelf: 'center',
+    marginTop : '30%'
+},
+  logo: {
+    width: 160,
+    height: 160,
+    alignSelf: 'center',
+    resizeMode : "contain"
+    
+
+  },
+  input: {
+    alignSelf : 'center',
+    marginTop : '20%',
+    backgroundColor : "#000000c0",
+    width: '80%',
+    textAlign:'left',
+    borderWidth: 1,
+    borderColor: '#000000c0',
+    color : 'white',
+    borderRadius : 5,
+    paddingHorizontal : 16,
+    paddingVertical: 8,
+  },
+  button: {
+    alignSelf : 'center',
+    marginTop : 24,
+    backgroundColor : "#000000c0",
+    width: 'auto',
+    borderWidth: 1,
+    borderRadius : 150/2,
+  },
+  textButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    textAlign: 'center',
+    color: 'white'
+  },
+  admin: {
+    alignSelf: 'center',
+    marginTop: 'auto',
+    marginBottom: 32,
+    backgroundColor : "#000000c0",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 150/2,
+  },
+});
+
+const color = StyleSheet.create({
+  white: {
+    color : 'white',
+},
+});
+
+export default Admin;
